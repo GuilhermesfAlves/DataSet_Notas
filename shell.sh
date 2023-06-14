91f0040f011b9e56b3c06a20734a7d1f3fa294cb
Guilherme <guilhermesfalves17@gmail.com>
2023-06-13 21:51:34 -0300

    13/06

---
+++ shell.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#função que remove que remove todas as linhas que façam parte de 2-2022
+remove_2_2022()
+{
+
+}
+
+#calculo do numero de individuos unicos para tal status
+qtd_ind_status()
+{
+
+}
+
+#máximo de vezes que um individuo cursou antes de ser aprovado
+#quantos individuos possuem o mesmo numero
+max_vez_cursado()
+{
+
+}
+
+#porcentagem aprovação e reprovação por ano
+perc_aprov_reprov_ano()
+{
+
+}
+
+#media de nota de aprovados por ano e periodo
+media_nota_aprov()
+{
+
+}
+
+#media de nota de reprovados por nota por ano e periodo
+media_nota_reprov()
+{
+
+}
+
+#media de frequencia dos reprovados por nota
+media_freq_reprov()
+{
+
+}
+
+#porcentagem de evasões total e anual
+perc_evasoes()
+{
+
+}
+
+#rendimento dos anos de pandemia em relação com os anteriores
+rend_pand()
+{
+
+}
+
+#rendimento dos alunos em 2022-1 em relação a pandemia e anos anteriores
+rend_after_pand()
+{
+
+}
