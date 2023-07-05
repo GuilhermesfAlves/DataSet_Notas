cd68ef25b3b92257cef2688cd684a15ad506162f
Guilherme <guilhermesfalves17@gmail.com>
2023-07-05 15:47:23 -0300

    05/07

--- shell.sh
+++ shell.sh
@@ -9,21 +9,26 @@ main()
     #qtd_ind_status $arq
     #max_vez_cursado $arq
     #perc_aprov_reprov_ano $arq
-    media_nota_aprov $arq
+    #media_nota_aprov $arq
+    #media_nota_reprov $arq
+    #media_freq_reprov $arq
+    
 }
 
 
 #função que remove que remove todas as linhas que façam parte de 2-2022
 function remove_2_2022()
 {
+    echo "Lista de pessoas que nao fazem parte do segundo semestre de 2022"
     arq=$1 
-    grep -v "2,2022" $arq 
+    grep -v "2,2022" $arq | grep -v matricula
 }
 
 # for i in {00..29}; do echo -n "$i "; echo $(./agenda  |   grep MEMBROS |grep "$i:OK"|wc -l) ; done 
 #calculo do numero de individuos unicos para tal status
 qtd_ind_status()
 { 
+    echo "Numero de individuos unicos para cada status"
     arq=$1
     temp="temp.txt"
     #remove equivalencia 2019, pega colunas grr e status, tira cabeçalho, remove linhas repetidas, joga em temp
@@ -37,6 +42,7 @@ qtd_ind_status()
 #quantos individuos possuem o mesmo numero
 max_vez_cursado()
 {
+    echo "Numero maximo de vezes que alguem cursou ALG1 e quantos obtiveram a mesma quantidade"
     arq=$1
     temp="temp.txt"
     #remove equivalencia 2019, tira cabeçalho, pega a primeira coluna de grrs, joga em temp.txt
@@ -53,6 +59,7 @@ max_vez_cursado()
 #porcentagem aprovação e reprovação por ano
 perc_aprov_reprov_ano()
 {
+    echo "Porcentagem de aprovados e reprovados em cada ano"
     arq=$1
     temp="temp.txt"
     #remove equivalencia 2019, tira cabeçalho, ordena por ano, pega as colunas de ano e status, remove os ainda matriculados
@@ -73,10 +80,8 @@ perc_aprov_reprov_ano()
         aprov=$(grep -c "Aprovado" $ano)
         reprov=$(grep -cv "Aprovado" $ano)
         total=$(expr $aprov + $reprov)
-        aprov=$(expr $aprov \* 100)
-        reprov=$(expr $reprov \* 100)
-        aprov=$(expr $aprov / $total)
-        reprov=$(expr $reprov / $total)
+        aprov=$(echo "scale = 2; $aprov * 100 / $total" | bc)
+        reprov=$(echo "scale = 2; $reprov * 100 / $total" | bc)
         
         echo "$aprov% aprovados e $reprov% reprovados em $i"
         rm $ano
@@ -85,53 +90,118 @@ perc_aprov_reprov_ano()
     rm $temp
 }
 
+media()
+{
+    arq=$1
+    status=$2
+    if [ $3 = 'freq' ]
+    then
+        arg=9
+    elif [ $3 = 'nota' ]
+    then
+        arg=8
+    fi
+    
+    temp="temp.txt"
+    #remove equivalencia, tira cabeçalho, separa as colunas grr:periodo:ano:nota:status, ordena pelos anos
+    grep -v EQUIVALENCIA $arq | grep -v matricula | cut -d, -f1,4,5,$arg,10 | sort -t, -k3 >> $temp
+    
+    temp1="temp1.txt"
+    cat $temp | grep $status >> $temp1
+    rm $temp
+ 
+    min=$(cut -d, -f3 $temp1 | head -n1)
+    max=$(cut -d, -f3 $temp1 | tail -n1)
+    
+    echo "ANO  mediaANO  media1p  media2p"
+    #calculo da porcentagem para cada ano
+    mediaG=0
+    for ((ano=$min; ano<=$max; ano++))
+    do
+        taxa1='taxa1sem.txt'
+        taxa2='taxa2sem.txt'
+
+        qtd1=$(grep -c "1,$ano" $temp1) 
+        qtd2=$(grep -c "2,$ano" $temp1)
+
+        grep "1,$ano" $temp1 | cut -d, -f1,2,4 >> $taxa1
+        grep "2,$ano" $temp1 | cut -d, -f1,2,4 >> $taxa2
+
+        soma1=0
+        soma2=0
+
+        for ((j=1; j<=$qtd1; j++))
+        do
+            nota=$(cut -d, -f3 $taxa1 | head -n"$j" | tail -n1)
+            soma1=$(expr $soma1 + $nota)
+        done
+        for ((j=1; j<=$qtd2; j++))
+        do
+            nota=$(cut -d, -f3 $taxa2 | head -n"$j" | tail -n1)
+            soma2=$(expr $soma2 + $nota)
+        done
+        
+        media1=0
+        media2=0
+
+        if (($qtd1 != 0))
+        then
+            media1=$(echo "scale = 0; $soma1 / $qtd1" | bc)
+        fi
+        if (($qtd2 != 0))
+        then
+            media2=$(echo "scale = 0; $soma2 / $qtd2" | bc)
+        fi
+
+        if (($media1 == 0)) && (($media2 != 0))
+        then
+            mediaT=$media2
+        elif (($media2 == 0)) && (($media1 != 0))
+        then 
+            mediaT=$media1
+        else
+            mediaT=$(echo "scale = 1; ($media1 + $media2) / 2" | bc)
+        fi       
+
+        echo -e "$ano\t$mediaT\t$media1\t$media2"
+        mediaG=$(echo "scale = 2; $mediaT + $mediaG" | bc )
+        rm $taxa1
+        rm $taxa2
+    done
+
+    mediaG=$(echo "scale = 2; $mediaG/($max - $min + 1)" | bc )
+    echo -e "\tMedia Total: $mediaG"
+
+    rm $temp1
+}
+
 #media de nota de aprovados por ano e periodo
 media_nota_aprov()
 {
-    arq=$1
-    temp="temp.txt"
-    #remove equivalencia 2019, tira cabeçalho, 
-    grep -v "CI1055,ALGORITMOS E ESTRUTURAS DE DADOS 1,1,2019,Sim,60,0,0,Aprovado,EQUIVALENCIA" $arq | grep -v "matricula" | cut -d, -f1,4,5,8,10 | sort -t, -k3 >> $temp
-    
-    min=$(grep Aprovado | cut -d, -f3 $temp | head -n1)
-    max=$(grep Aprovado | cut -d, -f3 $temp | tail -n1)
-    #calculo da porcentagem para cada ano
-    for ((ano=$min; ano<$max; ano++))
-    do
-        taxa='taxa.txt'
-        qtd=$(grep Aprovado | grep -c "$ano" $temp) 
-        echo "$qtd em $ano"
-        grep Aprovado | grep "$ano" $temp | cut -d, -f1,2,4 >> $taxa
-        echo "here"
-        soma=0
-        for ((j=0; j<$qtd; j++))
-        do
-            soma=$(expr $soma + $(cut -d, -f3 | head -n1))
-        done
-        echo "total: $soma em $ano"
-        cat $taxa
-        echo "--"
-        rm $taxa    
-    done
-    rm $temp
+    echo "Media de nota dos aprovados em cada ano, periodo e total"
+    media $1 Aprovado nota
 }
 
 #media de nota de reprovados por nota por ano e periodo
-#media_nota_reprov()
-#{
-
-#}
+media_nota_reprov()
+{
+    echo "Media de nota dos reprovados por nota em cada ano, periodo e total"
+    media $1 R-nota nota
+}
 
 #media de frequencia dos reprovados por nota
-#media_freq_reprov()
-#{
+media_freq_reprov()
+{
+    echo "Media de frequencia dos reprovados por nota por ano, periodo e total"
+    media $1 R-nota freq
+}
 
-#}
 
 #porcentagem de evasões total e anual
 #perc_evasoes()
 #{
-
+#    arq=$1
+#    grep -v EQUIVALENCIA $arq | grep -v matricula | 
 #}
 
 #rendimento dos anos de pandemia em relação com os anteriores
